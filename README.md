c744f7f91717ac387d10bee895358f7fe1898d7a
Den Williams <work@denwilliams.net>
2018-10-16 00:38:33 +1100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # xiaomi-uir-mqtt
+
 Xiaomi Mi Universal IR Remote to MQTT Bridge
+
+## Getting IP and Token
+
+```
+miio discover
+```
+
+See [miio](https://www.npmjs.com/package/miio)
+
+## Config
+
+```
+cp config.example.yml config.yml
+```
+
+...then edit.
+
+## Running
+
+```
+CONFIG_PATH=./config.yml node index.js
+```
---
+++ index.js
@@ -0,0 +1,104 @@
+const miio = require("miio");
+const mqttusvc = require("mqtt-usvc");
+const queue = require("queue");
+
+const service = mqttusvc.create();
+const { host, token } = service.config.gateway;
+let q, device, destroyPromise;
+
+service.on("message", (topic, data) => {
+  if (topic === "learn") {
+    addToQueue(learnCommand());
+  }
+  if (topic === "send") {
+    addToQueue(sendCommand(data));
+  }
+});
+
+service.subscribe("learn");
+service.subscribe("send");
+
+function learnCommand() {
+  return async function() {
+    console.log("Learning...");
+    const code = await learn();
+    service.send("learned", code);
+    console.log("Learned", code);
+  };
+}
+
+function sendCommand(code) {
+  return async function() {
+    console.log("Sending", code);
+    await send(code);
+  };
+}
+
+async function addToQueue(fn) {
+  if (q) {
+    q.push(fn);
+    return;
+  }
+
+  if (destroyPromise) {
+    await destroyPromise;
+    destroyPromise = undefined;
+  }
+
+  q = queue({ concurrency: 1 });
+  q.push(connectDevice);
+  q.push(fn);
+  q.start(err => {
+    destroyPromise = device.destroy();
+    q = undefined;
+  });
+}
+
+async function connectDevice() {
+  // console.log("Connect", host, token);
+  device = await miio.device({ address: host, token: token });
+  // console.log("Connected to", device.miioModel);
+}
+
+async function send(code) {
+  await device.call("miIO.ir_play", { freq: 38400, code });
+}
+
+function learn() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const learned = await device.call("miIO.ir_learn", { key: "100" });
+      console.log("Learning...", learned);
+    } catch (err) {
+      // if (err == "Error: Call to device timed out") {
+      //   that.platform.log.debug("[ERROR]irLearn - Remote Offline");
+      // } else {
+      //   that.platform.log.debug("[irLearn][ERROR] Error: " + err);
+      // }
+      return reject(err);
+    }
+
+    let interval = setInterval(async () => {
+      try {
+        const read = await device.call("miIO.ir_read", { key: "100" });
+        console.log("Waiting...", read);
+
+        if (read["code"] !== "") {
+          clearInterval(interval);
+          interval = undefined;
+
+          // console.log("Learned Code: " + read["code"]);
+          console.log("Learn Success!");
+          resolve(read["code"]);
+        }
+      } catch (err) {
+        if (err.message === "learn timeout") {
+          clearInterval(interval);
+          interval = undefined;
+          return resolve(null);
+        }
+        console.error("ERR", err);
+      }
+    }, 1000);
+  });
+}
